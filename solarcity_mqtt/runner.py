327e988cf0a084e9cbb1688d86615482fdb7c4c7
Gwendal Grignou <gwendal@gmail.com>
2017-03-25 16:05:57 -0700

    runner: fix indentation
    Signed-off-by: Gwendal Grignou <gwendal@gmail.com>

--- solarcity_mqtt/runner.py
+++ solarcity_mqtt/runner.py
@@ -12,34 +12,34 @@ kLog = logging.getLogger(__name__)
 
 
 class Runner(object):
-        def __init__(self, config_path):
-            self.config = RawConfigParser()
-            self.config.read(config_path)
-            self.observer = Observer()
-            self.event_handler = solarcity_mqtt.collector.SolarEventHandler(
-                config_path, self.recv_message)
-            self.mqtt = solarcity_mqtt.mqtt.MosquittoClient(config_path)
+    def __init__(self, config_path):
+        self.config = RawConfigParser()
+        self.config.read(config_path)
+        self.observer = Observer()
+        self.event_handler = solarcity_mqtt.collector.SolarEventHandler(
+        config_path, self.recv_message)
+        self.mqtt = solarcity_mqtt.mqtt.MosquittoClient(config_path)
 
-        def start(self):
-            path = self.config.get('data', 'path')
-            self.observer.schedule(
-                self.event_handler, path, recursive=False)
-            self.mqtt.start()
-            self.observer.start()
-            self.observer.join()
+    def start(self):
+        path = self.config.get('data', 'path')
+        self.observer.schedule(
+        self.event_handler, path, recursive=False)
+        self.mqtt.start()
+        self.observer.start()
+        self.observer.join()
 
-        def stop(self):
-            self.observer.stop()
-            self.mqtt.stop()
+    def stop(self):
+        self.observer.stop()
+        self.mqtt.stop()
 
-        def recv_message(self, values):
-            self.mqtt.publish(values)
+    def recv_message(self, values):
+        self.mqtt.publish(values)
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="""Listens on the serial port for power usage information,
-        and relays it to an MQTT server""")
+    description="""Listens on the serial port for power usage information,
+    and relays it to an MQTT server""")
     parser.add_argument('config', help='Path to configuration file')
     args = parser.parse_args()
     config = RawConfigParser()
@@ -49,10 +49,10 @@ def main():
     runner = Runner(args.config)
     runner.start()
     while True:
-        try:
-            time.sleep(5)
-        except KeyboardInterrupt:
-            break
+    try:
+        time.sleep(5)
+    except KeyboardInterrupt:
+        break
     runner.stop()
 
 if __name__ == '__main__':
